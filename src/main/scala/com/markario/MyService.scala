f7514e71e878bc980ebf5578866a24a39a9f4da7
Mark Zepeda <mark@hartsolution.com>
2015-10-27 17:45:59 -0700

    It works!

--- src/main/scala/com/markario/MyService.scala
+++ src/main/scala/com/markario/MyService.scala
@@ -12,71 +12,45 @@ import spray.json._
 import spray.routing._
 import spray.routing.directives.RouteDirectives
 
-// we don't implement our route structure directly in the service actor because
-// we want to be able to test it independently, without having to spin up an actor
-class MyServiceActor extends Actor with MyService {
-
-  // the HttpService trait defines only one abstract member, which
-  // connects the services environment to the enclosing actor or test
-  def actorRefFactory = context
-
-  // this actor only runs our route, but you could add
-  // other things here, like request stream processing
-  // or timeout handling
-  def receive = runRoute(myRoute)
-}
-
-
-// this trait defines our service behavior independently from the service actor
-trait MyService extends HttpService {
-
-  val myRoute =
-    path("api" / "int" / IntNumber / IntNumber) { (num, num2) =>
-      val intsTo = num to num2
-      val json = intsTo.toList.toJson
-      complete(json.toString)
-    }
-}
-
-trait ServicePathComponent {
+trait ServicePathComponent[L <: HList] {
   val servicePath: ServicePath
   trait ServicePath extends Directives{
-    def path[L <: HList]: Directive[L]
+    def path: Directive[L]
   }
 }
 
-trait ServiceRouteComponent {
+trait ServiceRouteComponent[L] {
   val serviceRoute: ServiceRoute
   trait ServiceRoute extends RouteDirectives{
-    def route[L <: HList]: (L => Route)
+    def route: (L => Route)
   }
 }
 
-trait ServiceActorComponent {
+trait ServiceActorComponent[L <: HList] {
   trait ServiceActor extends Actor with HttpService{
-    this: ServiceRouteComponent with ServicePathComponent=>
+    this: ServiceRouteComponent[L] with ServicePathComponent[L] =>
     override def receive: Actor.Receive = runRoute(servicePath.path.happly(serviceRoute.route))
     override def actorRefFactory = context
   }
 }
 
-trait ServiceRouteImpl extends ServiceRouteComponent{
+trait ServiceRouteImpl extends ServiceRouteComponent[shapeless.::[Int, shapeless.::[Int, HNil]]]{
   class IntRoute extends ServiceRoute{
-    override def route[L <: shapeless.::[Int, shapeless.::[Int, HNil]]] = { (list: L) =>
+    override def route = { (list) =>
       val intsTo = list.head to list.tail.head
       val json = intsTo.toList.toJson.toString
-      complete(json)
+      complete(json).asInstanceOf[Route]
     }
   }
 }
 
-trait ServicePathImpl extends ServicePathComponent {
+trait ServicePathImpl extends ServicePathComponent[shapeless.::[Int, shapeless.::[Int, HNil]]] {
   class IntPath extends ServicePath {
-    override def path[L <: shapeless.::[Int, shapeless.::[Int, HNil]]]: Directive[shapeless.::[Int, shapeless.::[Int, HNil]]] = path("api" / "int" / IntNumber / IntNumber)
+    override def path = path("api" / "int" / IntNumber / IntNumber)
   }
 }
 
-object IntService extends ServiceActorComponent{
+object IntService extends ServiceActorComponent[shapeless.::[Int, shapeless.::[Int, HNil]]]{
   class IntServiceActor extends ServiceActor with ServiceRouteImpl with ServicePathImpl{
     override val serviceRoute: ServiceRoute = new IntRoute
     override val servicePath: ServicePath = new IntPath
